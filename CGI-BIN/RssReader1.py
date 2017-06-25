d3742190c04b860b642a81261afc2888e9e54da5
Python <Python@ThinkPad>
2017-06-25 20:40:03 +0900

    Httpリーダー

---
+++ CGI-BIN/RssReader1.py
@@ -0,0 +1,32 @@
+#!/usr/bin.env python
+# conding: utf-8
+from RssParser import parse_rss
+from HttpHandler import Request, Response, get_htmltemplate
+import cgitb; cgitb.enable()            # (1)
+
+form_body=u"""
+<form method="POST" action="/cgi-bin/RssReader1.py">
+ RSSのURL:
+ <input type="text" size="40" name="url" value="%s"/>
+ <input type="submit" />
+</form>"""
+rss_parts=u"""
+<h3><a href="%(link)s">%(title)s</a><h3>
+<p>%(description)s</p>
+"""
+content=u"URLを入力してください"
+req=Request()
+if req.form.has_key('url'):
+    try:
+        rss_list=parse_rss(req.form['url'].value)
+        content=""
+        for d in rss_list:
+            content+=rss_parts%d
+    except:
+        pass
+
+res=Response()
+body=form_body%req.form.getvalue('url', '')
+body+=content
+rss.set_body(get_htmltemplate()%body)
+print res
--- Main.py
+++ Main.py
@@ -1,7 +1,5 @@
 import http.server
-
 server_address = ("", 8000)
-handler_class = http.server.CGIHTTPRequestHandler #1 ハンドラを設定
+handler_class = http.server.CGIHTTPRequestHandler
 server = http.server.HTTPServer(server_address, handler_class)
 server.serve_forever()
-
